33ef6e7d2f134d5c9af0cf4e82d044ebd539f13f
Nic0 <nicolas.caen@gmail.com>
2010-04-25 14:37:36 +0200

    socketAction fonction added and tested, bot seem to be stable 	modified:   main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-/*****************************
+/***************************************
 *       IRCBOT
 *           Nic0 (24/04/10)
-******************************/
+****************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,8 +14,9 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define PORT 6665
-#define HOST "senseii.tonbnc.fr"
+#define PORT 6667
+/*#define HOST "senseii.tonbnc.fr"*/
+#define HOST "irc.freenode.net"
 
 #define ERROR fprintf (stderr, \
         "%s:%d Error (%d) : %s\n", \
@@ -23,10 +25,12 @@
 
 int initSocket (int *sock, struct sockaddr_in *sockname, struct hostent *host_address);
 int initSocketConnect (int *sock);
+int socketAction (int *sock, char *buffer);
+int pong (int *sock, char *buffer);
 
 int main (void)
 {   
-    char buffer [2048] = {0};
+    /*char buffer [2048] = {0};*/
     struct sockaddr_in sockname;
     struct hostent *host_address;
     int sock = 0;
@@ -42,13 +46,12 @@ int main (void)
 
     while(1)
     {
+        char buffer [2048] = {0};
         recv(sock, buffer, sizeof(buffer) - 1, 0);
         printf("%s", buffer);
-        if (strstr(buffer, "ERROR :") != NULL)
-        {
-            close (sock);
-            return EXIT_FAILURE;
-        }
+
+        if ((socketAction (&sock, buffer)) == 1)
+            ERROR;
     }
     
     
@@ -77,7 +80,7 @@ int initSocket (int *sock, struct sockaddr_in *sockname, struct hostent *host_ad
  */
     sockname->sin_family = host_address ->h_addrtype;
     sockname->sin_port = htons (PORT);
-    memcpy ((char *) &((*sockname).sin_addr.s_addr), host_address->h_addr, host_address->h_length);
+    memcpy ((char *) &(sockname->sin_addr.s_addr), host_address->h_addr, host_address->h_length);
 
     if ((connect (*sock, (struct sockaddr *) sockname, sizeof (struct sockaddr_in))) == -1)
     {
@@ -93,7 +96,7 @@ int initSocketConnect (int *sock)
 
     char *nick = "NICK Hazardous\r\n";
     char *user = "USER Haz \"localhost\" \"irc_server\" :Nic0 s Bot\r\n";
-    char *join = "JOIN #test\r\n"; 
+    char *join = "JOIN #ubuntu,#archlinux,#linux\r\n"; 
 
     if ((send(*sock, nick, strlen(nick), 0)) == -1)
     {
@@ -112,6 +115,35 @@ int initSocketConnect (int *sock)
         ERROR;
         return 1;
     }
-
     return 0;
+}
+
+int pong (int *sock, char *buffer)
+{
+    buffer[1] = 'O';
+    if ((send(*sock, buffer, strlen(buffer), 0)) == -1)
+    {
+        ERROR;
+        return 1;
+    }
+    return 0;
+}
+
+int socketAction (int *sock, char *buffer)
+{
+    if (strstr(buffer, "PING :") != NULL && buffer[0] == 'P')
+    {
+        if ((pong(sock, buffer)) ==1)
+        {
+            close(*sock);
+            return 1;
+        }
+    }
+
+    if (strstr(buffer, "ERROR :") != NULL && buffer[0] == 'E')
+    {
+        close (*sock);
+        return 1;
+    }
+	return 0;
 }
