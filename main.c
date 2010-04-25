471bf25c8e4acb1ed18e530585a5b4ee267a3fcf
Nic0 <nicolas.caen@gmail.com>
2010-04-26 00:02:28 +0200

    host, port, and nick done and tested 	modified:   main.c 	bot.conf

--- main.c
+++ main.c
@@ -33,11 +33,13 @@ struct Configuration
 
 int initSocket (int *sock, struct sockaddr_in *sockname, 
                 struct hostent *host_address, struct Configuration *config);
-int initSocketConnect (int *sock);
+int initSocketConnect (int *sock, struct Configuration *config);
 int socketAction (int *sock, char *buffer);
 int pong (int *sock, char *buffer);
 int getConfiguration (struct Configuration *config);
 int extractConfig (char *element);
+char *concatString (char *str1, char *str2);
+
 
 
 int main (void)
@@ -49,11 +51,11 @@ int main (void)
 
     if ((getConfiguration (&config)) == 1)
         return EXIT_FAILURE;
-    printf("%s %d", config.host, config.port);
+    
     if ((initSocket (&sock, &sockname, host_address, &config)) == 1)
         return EXIT_FAILURE;
 
-    if (initSocketConnect (&sock) == 1) {
+    if (initSocketConnect (&sock, &config) == 1) {
         close (sock);
         return EXIT_FAILURE;
     }
@@ -105,14 +107,13 @@ int initSocket (int *sock, struct sockaddr_in *sockname,
 }
 
 
-int initSocketConnect (int *sock)
+int initSocketConnect (int *sock, struct Configuration *config)
 {
 
-    char *nick = "NICK Hazardous\r\n";
     char *user = "USER Haz \"localhost\" \"irc_server\" :Nic0 s Bot\r\n";
     char *join = "JOIN #test\r\n"; 
 
-    if ((send(*sock, nick, strlen(nick), 0)) != -1)
+    if ((send(*sock, config->nick, strlen(config->nick), 0)) != -1)
         if ((send(*sock, user, strlen(user), 0)) != -1)
             if ((send(*sock, join, strlen(join), 0)) != -1)
                 return 0;
@@ -154,6 +155,7 @@ int getConfiguration (struct Configuration *config)
     fichier = fopen ("bot.conf", "r");
     if (fichier != NULL) {
         while ((fgets (filebuff, MAXBUFF, fichier)) != NULL) {
+
             if (filebuff[0] == '#')
                 continue;
 
@@ -173,16 +175,26 @@ int getConfiguration (struct Configuration *config)
                 char *tmp = NULL;
                 tmp = strdup (strchr (filebuff, '"'));
 
-                if (extractConfig(tmp) == 1) {
-                    ERROR;
-                    return 1;
-                }
-                if ((config->port = atoi(tmp)) == 0) {
-                    ERROR;
-                    return 1;
-                }
-                continue;
+                if (extractConfig(tmp) == 0)
+                    if ((config->port = atoi(tmp)) != 0)
+                        continue;
+                ERROR;
+                return 1;
             }
+
+            if ((strstr (filebuff, "nick")) != NULL) {
+                char *tmp = NULL;
+                config->nick = strdup ("NICK ");
+                if ((tmp = strdup (strchr (filebuff, '"'))) != NULL)
+                    if (extractConfig (tmp) == 0)
+                        if ((concatString (config->nick, tmp)) != NULL)
+                            if ((concatString (config->nick, "\r\n")) != NULL)
+                                continue;
+                ERROR;
+                return 1;
+            }
+                
+                
 
         }
     }
@@ -196,18 +208,29 @@ int getConfiguration (struct Configuration *config)
 int extractConfig (char *element)
 {
     int i = 0;
-    do
-    {
+    do {
         element[i] = element[i+1];
         i++;
     }while (element[i+1] != '"' && i <= 1000);
 
-    if (i >= 1000)
-    {
-        printf("An error occured in the configuration file, a double-quote is missing");
+    if (i >= 1000) {
+        fprintf(stderr, "An error has occured in the configuration file, a double-quote is missing\n");
         return 1;
     }
     element[i] = '\0';
     return 0;
 }
 
+char *concatString (char *str1, char *str2)
+{
+    char *result;
+    int len1, len2;
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+ 
+    if(NULL == (result = realloc (str1, (len1 + len2 +1) * sizeof *result)))
+        return (NULL);
+ 
+    memcpy (result + len1, str2, len2 + 1);
+    return result;
+}
